7f9c130013027f599ea56745b78d2db6b75209de
Adam Gurley <adam.gurley@live.com>
2025-08-07 16:43:11 -0700

    🚀 Switch to serve for static file hosting instead of vite preview

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    host: true,
+    host: '0.0.0.0',
     port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
-    allowedHosts: ['all']
+    allowedHosts: ['all', '.ondigitalocean.app', 'glitchy-1-bqpkp.ondigitalocean.app']
   },
   server: {
     host: true,
